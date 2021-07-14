3767495eab4288e15dfd68eb41869b56b46beaab
rnlee1998 <1050145004@qq.com>
2021-07-14 10:48:26 +0800

    C语言学习静态成员章节

---
+++ cppProject1/cppProject1/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+class Person
+{
+public:
+
+	int m_height;
+	static int m_age;
+	static void func()
+	{
+		m_age = 1;
+		m_height = 5;
+	}	
+};
+
+int Person::m_age = 5;
+
+int main()
+{		
+	cout << Person::m_age << endl;
+	Person person;
+	cout << person.m_age << endl;
+	Person *p = new Person();
+	cout << p->m_age << endl;
+	delete p;
+	getchar();
+	return 0;
+}
+
