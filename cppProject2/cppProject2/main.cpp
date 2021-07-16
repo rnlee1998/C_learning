cc5013e741f925d996a036ebfe6cf58512e42579
rnlee1998 <1050145004@qq.com>
2021-07-16 14:30:12 +0800

    cppProject2更新为函数模板

--- cppProject2/cppProject2/main.cpp
+++ cppProject2/cppProject2/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 class Point
 {
 	friend Point operator+(Point p1, Point p2);
+	friend ostream  &operator<<(ostream &,const Point &);
 private:
 	
 	int m_x;
@@ -31,12 +32,25 @@ Point operator+(Point p1, Point p2)
 	return Point(p1.m_x + p2.getX(), p1.getY() + p2.getY());
 }
 
+ostream  &operator<<(ostream &cout,const Point &p)
+{
+	cout << "P(" << p.m_x << "," << p.m_y << ")" << endl;
+	return cout;
+}
+
+template <typename T>
+T add(T a, T b)
+{
+	return a + b;
+}
+
+
+
 int main()
 {
-	Point point1(1,0);
-	Point point2(2, 1);
-	Point point3 = point1+point2;
-	point3.display();
+	add<int>(10, 20);
+	add(0.5, 1.3);
+	add<Point>(Point(5, 5), Point(3, 2)).display();
 
 	return 0;
 }
