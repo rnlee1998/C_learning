13d59420798b235cdbf36e4e498c90e3ec436f08
rnlee1998 <1050145004@qq.com>
2021-07-30 14:38:36 +0800

    更新了链队列

---
+++ 链队列/链队列/main.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Qnode
+{
+public:
+	int data = 0;
+	Qnode* next = nullptr;
+
+};
+
+class Linkqueue
+{
+public:
+
+	Qnode* front;//队头指针
+	Qnode* rear;//队尾指针
+	Linkqueue()
+	{
+		front = new Qnode();
+		rear = front;
+	}
+	~Linkqueue()
+	{
+		while (front)
+		{
+			rear = front->next;
+			delete front;
+			front = rear;
+		}
+	}
+	void inQueue(int value);
+	void outQueue(int &value);
+};
+
+int main()
+{
+	Linkqueue linkqueue;
+	linkqueue.inQueue(5);
+	linkqueue.inQueue(3);
+
+	Qnode* p = linkqueue.front;
+	while (p)
+	{
+		cout << p->data << endl;
+		p = p->next;
+	}
+	cout << endl;
+	int value = -99;
+	linkqueue.outQueue(value);
+	cout << value << endl;
+	return 0;
+}
+
+void Linkqueue::inQueue(int value)
+{
+	rear->next = new Qnode();
+	rear = rear->next;
+	rear->data = value;
+}
+
+void Linkqueue::outQueue(int &value)
+{
+	if (!front->next) {
+		cout << "队空了，不能出队" << endl;
+		return;
+	}
+	Qnode* p = front->next;
+	front->next = p->next;
+	value = p->data;
+	delete p;
+}
---
+++ 队列_循环队列的顺序表示/队列_循环队列的顺序表示/main.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#define MAXQSIZE 100
+using namespace std;
+
+template <typename ElemType>
+class Queue
+{
+public:
+	ElemType* base;
+	int front;//队头下标
+	int rear;//队尾下标
+	Queue()
+	{
+		front = rear = 0;
+		base = new ElemType[MAXQSIZE]();
+	}
+	~Queue()
+	{
+		delete[] base;
+	}
+	int getLength();
+	void inQueue(ElemType value);//这里用少存一个元素的方式来求长度
+	void outQueue(ElemType &value);
+
+};
+
+
+
+int main()
+{
+	
+
+	Queue<int> queue;
+
+	cout << "队长为 = "<<queue.getLength() << endl;
+	for (int i = 1; i <= 100; i++) {
+
+		queue.inQueue(i);
+	}
+	cout << "队长为 = " << queue.getLength() << endl;
+	for (int i = 1; i <= 95; i++) {
+		int value;
+		queue.outQueue(value);
+	}
+	for (int i = 96; i <= 100; i++) {
+		int value;
+		queue.outQueue(value);
+		cout << "第" << i << "个入队的数 = " << value << endl;
+	}
+
+
+
+	return 0;
+}
+
+template <typename ElemType>
+int Queue<ElemType>::getLength()
+{
+	return (rear + MAXQSIZE - front) % MAXQSIZE;
+}
+
+template <typename ElemType>
+void Queue<ElemType>::inQueue(ElemType value)
+{
+	if ((rear + 1) % MAXQSIZE == front) {
+		cout << "队满了，不能入队" << endl;
+		return;
+	}
+	base[rear] = value;
+	rear =(rear+1)%MAXQSIZE;//实现了循环的列表 
+
+}
+template <typename ElemType>
+void Queue<ElemType>::outQueue(ElemType &value)
+{
+	if (rear == front) {
+		cout << "队空，不能出队" << endl;
+		return;
+	}
+	value = base[front];
+	front = (front + 1) % MAXQSIZE;
+
+}
+
